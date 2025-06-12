9039857295ad35463be5fb6beda390efcb09da38
sebastian <sebastian.adorsch@gmail.com>
2025-06-12 11:14:59 -0300

    init

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 const refreshAxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
